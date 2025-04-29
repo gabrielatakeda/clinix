cafe9833c0c2af573761711b8def942eb934262a
gabrielatakeda <gabrielatakedaa@gmail.com>
2025-04-28 22:14:43 -0300

    alteracoes

---
+++ src/main/java/org/example/Repository/ProntuarioRepository.java
@@ -0,0 +1,18 @@
+package org.example.Repository;
+
+import javax.persistence.EntityManager;
+
+public class ProntuarioRepository {
+
+    private EntityManager em;
+
+    public  ProntuarioRepository(){}
+
+    public ProntuarioRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    // O REPOSITORIO NAO ESTA PRONTO AINDA
+
+}
+  
